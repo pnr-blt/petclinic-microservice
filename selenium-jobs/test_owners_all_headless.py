0b81c40493e6faa98318971242af51e64d57fde1
pnr-blt <pmueb6647@gmail.com>
2021-11-15 17:51:35 +0000

    python files changed

--- selenium-jobs/test_owners_all_headless.py
+++ selenium-jobs/test_owners_all_headless.py
@@ -1,4 +1,4 @@
-rom selenium import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
